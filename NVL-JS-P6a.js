9ed89e2af2a2823957bb2f2e5ffca87b86b9f539
GuilleMF93 <54115816+GuilleMF93@users.noreply.github.com>
2019-09-05 13:13:36 +0200

    Add files via upload

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,5 @@
+var miNombre = "Guillermo Marcos Feijóo";
+var miEdad = 25;
+var diasSemana = ["lunes","martes", "miércoles", "jueves", "viernes", "sábado", "domingo"];
+var valGravedad = '9.8 m/s^2';
+var impares = ['1','3', '5'];
